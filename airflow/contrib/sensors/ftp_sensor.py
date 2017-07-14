b3fe49e983bf53460c2dd14dd185b16481013516
Michal <Michal@email.com>
2017-07-14 14:23:52 +0100

    [AIRFLOW-1413] Fix FTPSensor failing on unexpected 550 error message text

--- airflow/contrib/sensors/ftp_sensor.py
+++ airflow/contrib/sensors/ftp_sensor.py
@@ -26,10 +26,15 @@ class FTPSensor(BaseSensorOperator):
 
     :param path: Remote file or directory path
     :type path: str
+    :param fail_on_transient_errors: Fail on all errors, including 4xx transient errors. Default True.
+    :type fail_on_transient_errors: bool
     :param ftp_conn_id: The connection to run the sensor against
     :type ftp_conn_id: str
     """
     template_fields = ('path',)
+
+    """Errors that are transient in nature, and where action can be retried"""
+    transient_errors = [421, 425, 426, 434, 450, 451, 452]
 
     @apply_defaults
     def __init__(self, path, ftp_conn_id='ftp_default', *args, **kwargs):
@@ -37,10 +42,20 @@ class FTPSensor(BaseSensorOperator):
 
         self.path = path
         self.ftp_conn_id = ftp_conn_id
+        self.fail_on_transient_errors = fail_on_transient_errors
 
     def _create_hook(self):
         """Return connection hook."""
         return FTPHook(ftp_conn_id=self.ftp_conn_id)
+
+    def _get_error_code(self, e):
+        """Extract error code from ftp exception"""
+        try:
+            chunks = str(e).split(None, 1)
+            code = int(chunks[0])
+            return code
+        except ValueError:
+            return None
 
     def poke(self, context):
         with self._create_hook() as hook:
@@ -48,8 +63,10 @@ class FTPSensor(BaseSensorOperator):
             try:
                 hook.get_mod_time(self.path)
             except ftplib.error_perm as e:
-                error = str(e).split(None, 1)
-                if error[1] != "Can't check for file existence":
+                logging.info('Ftp error encountered: %s', str(e))
+                error_code = self._get_error_code(e)
+                if ((error_code != 550) and
+                    (self.fail_on_transient_errors or (not error_code in self.transient_errors))):
                     raise e
 
                 return False
